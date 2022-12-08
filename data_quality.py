f024db3be02b538a5de87758da3b8a39bdab40f8
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2022-12-08 22:39:43 +1300

    Update data_quality.py

--- data_quality.py
+++ data_quality.py
@@ -8,18 +8,21 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id = "",
-                 table= [],
+                 redshift_conn_id="",
+                 table="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id 
         self.table = table
+        self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        for t in self.table:
-            self.log.info(f'--DataQualityOperator checking {t}.')
-            sql = 'SELECT COUNT(*) FROM {t}'
-            records = redshift.get_records(sql)
-            self.log_info(records)
+        self.log.info('DataQualityOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {self.table} returned no results")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(f"Data quality check failed. {self.table} contained 0 rows")
+        logging.info(f"Data quality on table {self.table} check passed with {records[0][0]} records")
