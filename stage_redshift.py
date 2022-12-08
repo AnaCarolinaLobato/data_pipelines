567fee29e6cecc937bd1b3a6f479c70cd43ede85
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2022-12-08 22:37:02 +1300

    Update stage_redshift.py

--- stage_redshift.py
+++ stage_redshift.py
@@ -1,51 +1,67 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
     template_fields = ("s3_key",)
     copy_sql = """
-    COPY {} 
-    FROM '{}'
-    ACCESS_KEY_ID '{}'
-    SECRET_ACCESS_KEY '{}' 
- """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+    """
 
-    
     @apply_defaults
     def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # redshift_conn_id=your-connection-name
+                 json_path="",
                  redshift_conn_id="",
-                 aws_credentials_id= "",
-                 table = "",
-                 s3_bucket ="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
                  s3_key="",
-                 json_format = "",
+                 delimiter=",",
+                 ignore_headers=1,
                  *args, **kwargs):
 
-        
-        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id
-        self.aws_credentials_id = aws_credentials_id
-        self.table = table
-        self.s3_bucket = s3_bucket
-        self.s3_key= s3_key   
-        self.json_format= json_format
 
-    
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path = json_path
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
+
     def execute(self, context):
-        aws_hook = AwsHook(aws_conn_id=self.aws_credentials_id, verify=None)
-        redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id )
-        
-        self.log.info(f'Clearing records from {self.table}')
+        #self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
         redshift.run("DELETE FROM {}".format(self.table))
-        
-        self.log.info('Copying records from S3 to redshift')
+
+        self.log.info("Copying data from S3 to Redshift")
         rendered_key = self.s3_key.format(**context)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
-        sql = StageToRedshiftOperator.copy_sql.format(
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
             credentials.access_key,
             credentials.secret_key,
-            self.region,
-            self.json
+            self.ignore_headers,
+            self.delimiter
         )
-        
-        redshift.run(sql)
+        redshift.run(formatted_sql)
